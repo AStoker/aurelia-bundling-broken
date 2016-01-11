51c1168238bb40b34b526bbfb30c7257bbe256a3
Andrew Stoker <TheAndyBob@me.com>
2016-01-11 10:16:30 -0500

    Initial Checkin
    ASP.NET Folder Style. Initial Checkin

---
+++ build/tasks/bundle.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var bundler = require('aurelia-bundler');
+var bundles = require('../bundles.json');
+
+var config = {
+  force: true,
+  //packagePath: '.',
+  baseURL: './wwwroot',
+  configPath: './wwwroot/config.js',
+  bundles: bundles.bundles
+};
+
+gulp.task('bundle', function() {
+  return bundler.bundle(config);
+});
+
+gulp.task('unbundle', function() {
+  return bundler.unbundle(config);
+});
